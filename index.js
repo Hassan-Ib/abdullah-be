6dc957836ad7230975af47d0163b567e0d6075c3
ibrahim hassan <ibrahim.ayomide@mariopsys.com>
2026-04-18 21:39:26 +0100

    bot'

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+
+const app = express();
+
+// cors middleware
+app.use((req, res, next) => {
+  // accepts request from 5500 and 5173 (frontend dev servers)
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
+
+const yfApis = {
+  bars: ({ symbol, interval, range }) =>
+    `https://query1.finance.yahoo.com/v8/finance/chart/${encodeURIComponent(symbol)}?interval=${interval}&range=${range}&includePrePost=false`,
+
+  quickPrices: (
+    batch,
+    fields = "fields=regularMarketPrice,regularMarketPreviousClose,regularMarketChangePercent",
+  ) =>
+    `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${encodeURIComponent(batch)}&fields=${fields}`,
+};
+app.get("v1/api/stock", async (req, res) => {
+  const symbol = req.query.symbol || "AAPL";
+  const interval = req.query.interval || "1m";
+  const range = req.query.range || "2d";
+  const response = await fetch(yfApis.bars({ symbol, interval, range }));
+  const data = await response.json();
+  console.log(data);
+  res.json(data);
+});
+
+app.get("v2/api/quick", async (req, res) => {
+  const batch = req.query.batch || "AAPL,MSFT,GOOGL";
+  const fields =
+    req.query.fields ||
+    "regularMarketPrice,regularMarketPreviousClose,regularMarketChangePercent";
+  const response = await fetch(yfApis.quickPrices(batch, fields));
+  const data = await response.json();
+  console.log(data);
+  res.json(data);
+});
+
+app.listen(5200, () => console.log("Server running on port 5200"));
